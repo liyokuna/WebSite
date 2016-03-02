e47061dc713b6df45c5f0f46dd642b078fbc629b
Lionel MUKUNA CIOWELA <liyokuna@yahoo.fr>
2016-03-01 21:51:05 -0600

    Personnal Web site Code
    My personnal Web site codes are here!
    php send mail

---
+++ Personnal Web Site/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lionel MUKUNA CIOWELA</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link  rel="icon" href="http://lmukunaciowela.pe.hu/www/pictures/Pretty-Office-Contact.ico  " 
+  type="image/vnd.microsoft.ico" />
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!--Latest compiled and minified CSS from PureCSS-->
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<style>
+body {
+    background-color: #0d0d0d;
+}
+.centre{
+	
+	width:50%;
+	top: 50%;
+	left: 25%;
+	position: absolute;
+}
+
+</style>
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+<div class="centre">
+
+	<div class="row">
+	
+		<div class="col-sm-4 col-sm-offset-4">
+			<p>
+		<a href="english.php">
+			<button type="button" class="btn btn-default btn-lg btn-block" >English</button>
+		</a>
+			</p>
+		</div>
+	
+	</div>
+	<div class="row">
+	
+		<div class="col-sm-4 col-sm-offset-4">
+			<p>
+		<a href="francais.php">
+			<button type="button" class="btn btn-default btn-lg btn-block">Français</button>
+		</a>
+			</p>
+		</div>
+	</div>
+	
+</div>
+</div>
+
+</body>
+</html>
